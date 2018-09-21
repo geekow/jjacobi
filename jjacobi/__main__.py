96ff9530962a4749dc524dab47ef6471ab53c472
Jean Jacobi <jean.jacobi@live.de>
2018-09-21 17:16:51 +0200

    404 added

--- jjacobi/__main__.py
+++ jjacobi/__main__.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello, world!"
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 def run():
